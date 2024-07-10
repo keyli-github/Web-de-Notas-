a5318c203d05d79451605aae48ae513bd6dff739
keyli-github <keyli.roncal@tecsup.edu.pe>
2024-07-10 11:15:13 -0500

    version1Notas
    Esta es mi primera versión de notas

---
+++ script.js
@@ -0,0 +1,16 @@
+function addNote(){
+
+    const noteInput = document.getElementById('noteInput');
+
+    const noteList = document.getElementById('noteList');
+
+
+if(noteInput.value.trim() !==""){
+    const newNote = document.createElement('li');
+    newNote.textContent = noteInput.value;
+    noteList.appendChild(newNote);
+    noteInput.value = "";
+    }else{
+        alert('Por favor, escriba una nota antes de agregar :)')
+    }
+    }
